ca4314301c317a84565e2f05b412dc3d8af65e10
Arun <arunchandran619@gmail.com>
2019-01-04 01:30:04 +0530

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { ComponentsModule } from './components/components.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
